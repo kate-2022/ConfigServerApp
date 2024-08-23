a878e401768f7c6defe573fcecce53d76ff01d8e
kate <Katrin.Schaak@gmx.de>
2024-08-23 16:10:14 +0200

    ConfigServerAppForTesting

---
+++ src/test/java/com/_s2h/ks/SbConfigServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com._s2h.ks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbConfigServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
